241e94aaf9b0d65e713f42f6cc98aa4cd13449a6
Giedre <giedre.klima@gmail.com>
2022-08-04 11:05:53 +0300

    feat(66) 6 tasks done

--- public/js/main.js
+++ public/js/main.js
@@ -19,4 +19,67 @@ console.log('replaceSpacesWithDash()', replaceSpacesWithDash('labas vakaras'));
 const calcAvg = (arr) => arr.reduce((sum, num) => sum + num, 0) / arr.length;
 console.log('calcAvg', calcAvg(numbersArr));
 const avg = numbersArr.reduce((acc, curr) => acc + curr) / numbersArr.length;
+console.group('1. Sukurkite funkciją, kuri atspausdiną tekstą didžiosiomis raidėmis');
+{
+    const allToUpperCase = (word) => word.toUpperCase();
+    console.log(allToUpperCase('sun shine rain'));
+}
+console.groupEnd();
+console.group('2. Sukurkite funkciją, kuri grąžina pirmo ir antro parametro bendrą simbolių skaičių');
+{
+    const lengthOfTwo = (word1, word2) => word1.length + word2.length;
+    console.log(lengthOfTwo('labas', 'rytas'));
+}
+console.groupEnd();
+console.group('3. Sukurkite funkciją, kuri grąžina <true>, jeigu žodyje yra 2 parametru perduoda raidė, priešingu atveju false');
+{
+    const hasLetter = (text, letter) => text.includes(letter);
+    console.log(hasLetter('text', 'x'));
+    console.log(hasLetter('text', 'a'));
+}
+console.groupEnd();
+console.group('4. Sukurkite funkciją, kuri grąžina <true>, jeigu žodyje yra lyginis skaičius simbolių');
+{
+    const isEvenSymbols = (term) => term.length % 2 === 0;
+    console.log(isEvenSymbols('sun'));
+    console.log(isEvenSymbols('rain'));
+}
+console.groupEnd();
+console.group('5. Sukurkite funkciją, kuri grąžina balsių kiekį žodyje');
+{
+    const howManyVowel = (vowelWord) => {
+        const vowels = ['a', 'e', 'i', 'y', 'o', 'u'];
+        let count = 0;
+        for (const char of vowelWord) {
+            if (vowels.includes(char)) {
+                count++;
+            }
+        }
+        return count;
+    };
+    console.log('labasa', howManyVowel('labasa'));
+}
+console.groupEnd();
+console.group('6. Sukurkite funkciją, kuri grąžina bet kokios raidės kiekį žodyje');
+{
+    const randomLetterCount = (word5) => {
+        const wordLength = word5.length;
+        const randomIndex = (max, min) => Math.floor(Math.random() * (max - min + 1) + min);
+        const randomWordIndex = randomIndex(wordLength, 0);
+        const letter = word5.charAt(randomWordIndex);
+        const letterArr = word5.split('');
+        let countOfLetter = 0;
+        letterArr.forEach((item) => {
+            if (item === letter)
+                countOfLetter += 1;
+        });
+        return [randomWordIndex, countOfLetter];
+    };
+    console.log('arbitrary [index, count]', randomLetterCount('arbitrary'));
+}
+console.groupEnd();
+console.groupCollapsed('7. Sukurkite funkciją, kuri ištrintų pirmą surastą simbolį žodyje ir grąžintų pakeistą žodį');
+{
+}
+console.groupEnd();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+/* eslint-disable no-lone-blocks */
+/* eslint-disable no-empty */
+/* eslint-disable no-console */
+
 // import { add } from "./helpers/calc";
 
 const add = (a: number, b: number): number => a + b;
@@ -31,3 +35,81 @@ const calcAvg = (arr: number[]): number => arr.reduce((sum, num) => sum + num, 0
 console.log('calcAvg', calcAvg(numbersArr));
 
 const avg = numbersArr.reduce((acc, curr) => acc + curr) / numbersArr.length;
+
+// 08-04 uzdaviniu sprendimai
+
+console.group('1. Sukurkite funkciją, kuri atspausdiną tekstą didžiosiomis raidėmis');
+{
+  const allToUpperCase = (word: string): string => word.toUpperCase();
+  console.log(allToUpperCase('sun shine rain'));
+}
+console.groupEnd();
+
+console.group('2. Sukurkite funkciją, kuri grąžina pirmo ir antro parametro bendrą simbolių skaičių');
+{
+  const lengthOfTwo = (word1: string, word2: string): number => word1.length + word2.length;
+  console.log(lengthOfTwo('labas', 'rytas'));
+}
+console.groupEnd();
+
+console.group('3. Sukurkite funkciją, kuri grąžina <true>, jeigu žodyje yra 2 parametru perduoda raidė, priešingu atveju false');
+{
+  const hasLetter = (text: string, letter: string): boolean => text.includes(letter);
+  console.log(hasLetter('text', 'x'));
+  console.log(hasLetter('text', 'a'));
+}
+console.groupEnd();
+
+// 10 min
+console.group('4. Sukurkite funkciją, kuri grąžina <true>, jeigu žodyje yra lyginis skaičius simbolių');
+{
+  const isEvenSymbols = (term: string): boolean => term.length % 2 === 0;
+  console.log(isEvenSymbols('sun'));
+  console.log(isEvenSymbols('rain'));
+}
+console.groupEnd();
+
+// 15 min
+console.group('5. Sukurkite funkciją, kuri grąžina balsių kiekį žodyje');
+{
+  const howManyVowel = (vowelWord: string): number => {
+    const vowels = ['a', 'e', 'i', 'y', 'o', 'u'];
+    let count = 0;
+    for (const char of vowelWord) {
+      if (vowels.includes(char)) {
+        count++;
+      }
+    }
+    return count;
+  };
+  console.log('labasa', howManyVowel('labasa'));
+}
+console.groupEnd();
+
+// 15 min
+console.group('6. Sukurkite funkciją, kuri grąžina bet kokios raidės kiekį žodyje');
+{
+  const randomLetterCount = (word5 : string): number[] => {
+    const wordLength = word5.length;
+    const randomIndex = (max: number, min: number): number => Math.floor(Math.random() * (max - min + 1) + min);
+    const randomWordIndex = randomIndex(wordLength, 0);
+
+    const letter = word5.charAt(randomWordIndex);
+    const letterArr = word5.split('');
+    let countOfLetter = 0;
+    letterArr.forEach((item) => {
+      if (item === letter) countOfLetter += 1;
+    });
+
+    return [randomWordIndex, countOfLetter];
+  };
+  console.log('arbitrary [index, count]', randomLetterCount('arbitrary'));
+}
+console.groupEnd();
+
+// 15 min
+console.groupCollapsed('7. Sukurkite funkciją, kuri ištrintų pirmą surastą simbolį žodyje ir grąžintų pakeistą žodį');
+{
+
+}
+console.groupEnd();
